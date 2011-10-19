4376f24229addb75ad9a73495f6429bcb55ae0e6
Mark Huot <mark@markhuot.com>
2011-10-19 18:59:35 -0400

    adding config

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+// Name: The human readable name of the add-on. This is what
+// appears in the add-ons tab of EE.
+define('GEODE_NAME', 'Geode');
+
+// Id: Used to reference the class name where relevant (database
+// queries and accessory `id` attribute).
+define('GEODE_SHORT_NAME', 'geode');
+
+// Description: Displayed in the add-ons tab to help users know
+// what they are installing.
+define('GEODE_DESCRIPTION', 'Gecoding for EE2');
+
+// Version: The version number of the add-on. It should follow
+// the same major.minor.bugfix scheme that EE follows.
+define('GEODE_VERSION', '1.0.1');
+
+// URL: The URL users can go to for help
+define('GEODE_URL', 'github.com/markhuot/geode.ee_addon');
